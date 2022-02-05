368e3e59adf77c6a2d246488529cccb78cbd69d3
sourtaste000 <me@sourtaste000.dev>
2022-02-05 15:19:13 -0800

    not using either

--- backend/src/status_check/mod.rs
+++ backend/src/status_check/mod.rs
@@ -1,25 +1,24 @@
-use either::*;
 use neon::prelude::*;
 
-fn get_status(url: &str) -> Either<bool, String> {
+fn get_status(url: &str) -> String {
     match reqwest::blocking::Client::new().get(url).send() {
         Ok(_) => {
-            return Left(true);
+            return String::new();
         }
         Err(e) => {
-            return Right(format!("{}", e));
+            return format!("{}", e).into();
         }
     };
 }
 
 pub fn mojang_auth_status(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string(get_status("https://authserver.mojang.com").right_or_default()))
+    Ok(cx.string(get_status("https://authserver.mojang.com")))
 }
 
 pub fn mojang_session_status(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string(get_status("https://sessionserver.mojang.com").right_or_default()))
+    Ok(cx.string(get_status("https://sessionserver.mojang.com")))
 }
 
 pub fn mojang_texture_status(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string(get_status("https://textures.minecraft.net").right_or_default()))
+    Ok(cx.string(get_status("https://textures.minecraft.net")))
 }
